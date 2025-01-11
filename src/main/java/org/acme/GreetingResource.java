bd295d63f14720431281a54889866c9d5013fe33
Juste Bouithy <jrb518@ensign.edu>
2025-01-10 18:34:32 -0700

    first commit second time

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,31 @@
 package org.acme;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
-
-    @GET
+    @POST
+    @Path("/personalized")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public String personalizedHelloPost(Person p) {
+        return "Hello " + p.getFirst() + " " + p.getLast();
+    }
+    public static class Person {
+        private String first;
+        private String last;
+
+        public String getFirst() {
+            return first;
+        }
+        public void setFirst(String first) {
+            this.first = first;
+        }
+        public String getLast() {
+            return last;
+        }
+        public void setLast(String last) {
+            this.last = last;
+        }
     }
 }
